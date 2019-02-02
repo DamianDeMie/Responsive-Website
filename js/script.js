4bc76e9f5368765014e46418530ceae8b3910fdc
Damian <damiandemie@gmail.com>
2019-02-02 14:38:00 +0100

    Added required files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function dropdown1() {
+  document.getElementById("dropdown-1").classList.toggle("show");
+}
+
+function dropdown2() {
+  document.getElementById("dropdown-2").classList.toggle("show");
+}
